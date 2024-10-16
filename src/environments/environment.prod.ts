f679bbc5fd6f4199d2678a967fe2f6f10a378c01
Dmitriy <148681959+phoenix-intensive@users.noreply.github.com>
2024-10-16 12:33:23 +0700

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  api: "https://web-studio-backend-vercel.vercel.app/api/",
+  serverStaticPath: "https://web-studio-backend-vercel.vercel.app/images/"
+};
